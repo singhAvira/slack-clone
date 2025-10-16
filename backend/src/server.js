97ae3228e8792c05a7dfec8fa08a390d4a17a386
Aviral Singh <aviral1920singh@gmail.com>
2025-10-16 21:36:14 +0530

    intial commit

---
+++ backend/src/server.js
@@ -0,0 +1,11 @@
+import express from "express";
+import { ENV } from "./config/env.js"
+
+const app=express();
+
+
+app.get("/",(req,res)=>{
+    res.send("hello World");
+})
+
+app.listen(ENV.PORT,()=> console.log("Server started on port: ",ENV.PORT));
