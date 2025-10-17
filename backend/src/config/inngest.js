994d87b87acfe95a02266f5a06aaac00435e2cb7
Aviral Singh <aviral1920singh@gmail.com>
2025-10-17 12:29:08 +0530

    auth implemented

---
+++ backend/src/config/inngest.js
@@ -0,0 +1,40 @@
+import { Inngest } from "inngest";
+import { connectDB } from "./db";
+
+// Create a client to send and receive events
+export const inngest = new Inngest({ id: "slack-site" });
+
+//now we craete a function which is being used to create an user or dlete user
+//in database
+const syncUser=inngest.createFunction(
+    {id: "sync-user"},
+    {event: "clerk/user.created"},
+    async({event})=>{
+     await connectDB()
+
+     const {id, email_addresses,first_name,last_name,image_url }=event.data;
+
+     const newUser={
+        clerkId:id,
+        email:email_addresses[0]?.email_address,
+        name: `${first_name ||""} ${last_name || ""}`,
+        image:image_url,
+     }
+
+     await User.create(newUser);
+
+     //TODO DO THE MORE THING
+    }
+)
+
+const deleteUserFromDB = inngest.createFunction(//delete the user from the databse 
+    {id: "delete-user-from-db"},
+    {event: "clerk/user.deleted"},
+    async ({ event }) =>{ //this data is coming from clerk
+        const {id }=event.data;
+        await User.deletOne({clerkId:id});
+    }
+)
+
+// Create an empty array where we'll export future Inngest functions
+export const functions = [syncUser,deleteUserFromDB];
